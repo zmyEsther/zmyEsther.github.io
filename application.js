506334a771dd2f1ee46a992640befd7adbd20bfb
zmyEsther <77343471+zmyEsther@users.noreply.github.com>
2021-03-18 21:08:44 +0000

    Added queries

--- application.js
+++ application.js
@@ -1,17 +1,1 @@
 console.log("Hello world");
-function test()
-{
-	var returnData;
-	$.ajax({
-		type: "GET",
-		url: "https://raw.githubusercontent.com/zmyEsther/zmyEsther.github.io/main/resources/data.ttl",
-		dataType: "text",
-		async: false,
-		success: function(response)
-		{
-			returnData = response;
-		}
-	});
-	return returnData;
-}
-console.log(select(test(),null,null,'<Country>'));
